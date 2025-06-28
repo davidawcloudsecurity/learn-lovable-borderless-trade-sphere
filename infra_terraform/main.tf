2e60a5a28737c4437911494a05400eb23fc27709
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-06-28 20:38:12 +0800

    Update main.tf - update nginx to handle api to backend

--- infra_terraform/main.tf
+++ infra_terraform/main.tf
@@ -329,6 +329,13 @@ resource "aws_instance" "nginx" {
                       proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
                       proxy_set_header X-Forwarded-Proto \$scheme;
                   }
+
+                  location /api/ {
+                    proxy_pass http://${aws_instance.mysql.private_ip}:3001;
+                    proxy_http_version 1.1;
+                    proxy_set_header Host $host;
+                    proxy_set_header X-Real-IP $remote_addr;
+                  }
               }
               EOF1
 
