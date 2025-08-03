91b1976e293a5e9f997dc25b887004906839b090
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-08-03 15:52:45 +0800

    Update main.tf - install docker for ubuntu

--- infra_terraform/main.tf
+++ infra_terraform/main.tf
@@ -465,6 +465,12 @@ resource "aws_launch_template" "mysql" {
     npm install -y express cors
     npm install pg @types/pg
     node server.js &
+    apt update -y
+    apt install apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+    apt-cache policy docker-ce
+    apt install docker-ce
     docker run -d \
       -e POSTGRES_DB=wordpress \
       -e POSTGRES_USER=wordpress \
