c8a6c25aff40cb9c28f3b6ee702cc03796fa5db8
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-08-08 21:26:11 +0800

    Update start.sh

--- infra_terraform/start.sh
+++ infra_terraform/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo yum install -y yum-utils shadow-utils; sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo; sudo yum -y install terraform
 echo 'alias tf="terraform"; alias tfa="terraform apply --auto-approve"; alias tfd="terraform destroy --auto-approve"; alias tfm="terraform init; terraform fmt; terraform validate; terraform plan"' >> ~/.bashrc
 source ~/.bashrc
-sudo yum install -y yum-utils shadow-utils; sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo; sudo yum -y install terraform; terraform init
+terraform init
