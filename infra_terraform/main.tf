983a68167bc81cc53a439b956a4415b4b4da7a67
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-08-17 19:17:16 +0800

    Update main.tf update elb sg to allow port 80

--- infra_terraform/main.tf
+++ infra_terraform/main.tf
@@ -222,8 +222,8 @@ resource "aws_security_group" "private_app" {
 
   ingress {
     description = "HTTP from public subnet"
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     #    cidr_blocks = [aws_security_group.public_facing.id]
     security_groups = [aws_security_group.public_facing.id]
