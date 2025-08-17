27ba5d08dde65e69a44235ab254868a1b37e7789
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-08-17 17:43:26 +0800

    Update main.tf - remove engine version

--- infra_terraform/main.tf
+++ infra_terraform/main.tf
@@ -611,7 +611,7 @@ resource "aws_db_instance" "postgres" {
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "postgres"
-  engine_version       = "15.4"
+#  engine_version       = "15.4"
   instance_class       = "db.t3.micro"
   db_name              = "wordpress"
   username             = "wordpress"
